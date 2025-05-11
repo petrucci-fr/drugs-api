20f7da21d2bc136a1778b006d62b39ae4f1f4490
Franco Petrucci <petrucci.fr@gmail.com>
2025-05-10 22:02:56 -0300

    Modified settings.py

--- src/settings.py
+++ src/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 SERVICE_ACCOUNT_FILE = Path('src\credentials.json')
 SPREADSHEET_ID = '13QqX10qsfU2UH-z40rM4Qe2A6YLoTPxh5INQJusf7-A'
